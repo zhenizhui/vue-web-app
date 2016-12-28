d6e8f2f542d0ad0a40bd39fb632dece6dd5d1937
zhenizhui <zhuinizhe@gmail.com>
2016-12-28 20:20:40 +0800

    add swiper components, which come from github: wangdahoo/vonic

---
+++ src/components/swiper/SwiperPagination.vue
@@ -0,0 +1,68 @@
+<template>
+  <div v-if="circles.length > 0" class="swiper-pagination">
+    <span v-for="($index, c) in circles"
+          class="circle"
+          :style="{
+            'backgroundColor': pagerColor,
+            'opacity': activeIndex == $index ? 1 : 0.4
+          }"></span>
+  </div>
+</template>
+<style lang="stylus" rel="stylesheet/stylus">
+  .swiper-pagination
+    width: 100%
+    height: 20px
+    left: 0
+    margin-top: -20px
+    line-height: 16px
+    .circle
+      display: inline-block;
+      width: 7px;
+      height: 7px;
+      border-radius: 50%;
+      margin-right: 4px;
+      &:last-of-type
+        margin-right: 0;
+</style>
+<script>
+  const reColor = /^#([0-9A-Fa-f]{3})|([0-9A-Fa-f]{6})$/;
+
+  export default {
+    props: {
+      size: {
+        type: Number,
+        default: 0
+      },
+      pagerColor: {
+        type: String,
+        default: '#333',
+        validator (v) {
+          return reColor.test(v);
+        }
+      }
+    },
+    data () {
+      return {
+        circles: [],
+        activeIndex: 0
+      };
+    },
+    ready () {
+
+    },
+    methods: {
+      init () {
+        let circles = [];
+        for (let i = 0; i < this.size; i++) {
+          circles.push(i);
+        }
+
+        this.circles = circles;
+      },
+
+      activate (index) {
+        this.activeIndex = index;
+      }
+    }
+  };
+</script>
