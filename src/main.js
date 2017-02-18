d57668996c03fd34b20edf345d28fd5ef7182c79
zhenizhui <zhuinizhe@gmail.com>
2017-02-18 21:41:07 +0800

    first, make Routers lazy loading second, add user's router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import app from './App';
-// index 首屏
-import index from './components/index/index.vue';
-// seller 商家页面
-import seller from './components/seller/seller.vue';
-// 商品详情页
-// import foodDetail from './components/fooddetail/fooddetail.vue';
 import VueResource from 'vue-resource';
 import './css/reset.css';
 import './css/font-awesome.css';
@@ -23,14 +17,23 @@ let router = new VueRouter({
 
 router.map({
   '/': {
-    component: index
+    component: require('./components/index/index.vue')
   },
   '/index': {
-    component: index
+    component: require('./components/index/index.vue')
   },
   '/seller/:seller': {
     name: 'seller',
-    component: seller
+    component: require('./components/seller/seller.vue'),
+    subRoutes: {
+      '/': {
+        component: require('./components/sellergood/sellergood.vue')
+      }
+    }
+  },
+  '/user': {
+    name: 'user',
+    component: require('./components/user/user.vue')
   }
 });
 
